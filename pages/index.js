773edd82916058d662f56b18796aef84f79779d0
Robin Millette <robin@millette.info>
2019-09-20 14:36:26 -0400

    Display mdx component we fetch as text.

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import 'isomorphic-unfetch'
+import MDX from '@mdx-js/runtime'
+import { MDXProvider } from '@mdx-js/react'
+
+const FrontPage = ({ MDXContent }) => (
+  <MDXProvider>
+    <div>
+      <p>Hello Earth</p>
+      <MDX>{MDXContent}</MDX>
+    </div>
+  </MDXProvider>
+)
+
+FrontPage.getInitialProps = async ({ req }) => {
+  // console.log('FrontPage.getInitialProps') // , a, b, c)
+  const res = await fetch('http://localhost:3000/c1.txt')
+  const c = await res.text()
+  // console.log('CCC', c)
+  // return { MDXContent: '### more stuff' }
+  return { MDXContent: c }
+}
+
+export default FrontPage
+ 
